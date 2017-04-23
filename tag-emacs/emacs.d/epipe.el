cc0979e561b98d27bd6e640e185f07dea0f18929
jmn <norlund@gmail.com>
2017-04-23 16:49:34 +0200

    comment

--- tag-emacs/emacs.d/epipe.el
+++ tag-emacs/emacs.d/epipe.el
@@ -1,6 +1,5 @@
-;; This buffer is for notes you don't want to save, and for Lisp evaluation.
-;; If you want to create a file, visit that file with C-x C-f,
-;; then enter the text in that file's own buffer.
+;; epipe.el
+
 (defun pager-read-pipe (fname)
   (let ((buf (generate-new-buffer "*pager*"))
         (pname (concat "pager-" fname)))
