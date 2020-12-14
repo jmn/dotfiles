4d305b5d954a2fe53c7d248806486ef16695c67b
jmn <norlund@gmail.com>
2020-12-14 20:11:24 +0100

    Custom

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(custom-enabled-themes '(poet-dark))
+ '(custom-enabled-themes '(poet-dark))
  '(custom-safe-themes
    '("28a104f642d09d3e5c62ce3464ea2c143b9130167282ea97ddcc3607b381823f" "35c096aa0975d104688a9e59e28860f5af6bb4459fd692ed47557727848e6dfe" "2d035eb93f92384d11f18ed00930e5cc9964281915689fa035719cab71766a15" default))
  '(fill-column 120)
